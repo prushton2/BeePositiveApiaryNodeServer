f65f38ba9265da1798b150c13a8774d70a82ff14
prushton <46426283+prushton2@users.noreply.github.com>
2022-02-15 18:30:23 +0000

    changed Example app to App

--- index.js
+++ index.js
@@ -110,5 +110,5 @@ app.get("/reset/*", async(req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
