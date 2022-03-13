007d45e29003a191102578086bad56ef1cb8da73
prushton2 <peterrushton418@gmail.com>
2022-03-13 15:09:12 -0400

    disabled sequelize logging

--- database.js
+++ database.js
@@ -6,6 +6,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize("bpa-db", "user", "pass", {
   dialect: "sqlite",
   host: "./bpa.sqlite",
+  logging: false
 })
 
 module.exports = sequelize
