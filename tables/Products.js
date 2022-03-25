b53aa430130518592852b33e877ed8a8d8462717
prushton2 <peterrushton418@gmail.com>
2022-03-25 19:05:02 -0400

    now uses upsert to update the table

--- tables/Products.js
+++ tables/Products.js
@@ -41,7 +41,8 @@ productList = {
         "price"        :1,
         "unit"         :"&nbsp&nbsp&nbsp",
         "description"  :"",
-        "increment"    :0
+        "increment"    :0,
+        "isInStock"    :true
     },
     1: {
         "id"           :100,
@@ -49,7 +50,8 @@ productList = {
         "price"        :4.99,
         "unit"         :"&nbsp&nbsp&nbsp",
         "description"  :"Cuticle Salve",
-        "increment"    :1
+        "increment"    :1,
+        "isInStock"    :true
     },
     2: {
         "id"           :101,
@@ -57,7 +59,8 @@ productList = {
         "price"        :2.99,
         "unit"         :"&nbsp&nbsp&nbsp",
         "description"  :"Spa",
-        "increment"    :1
+        "increment"    :1,
+        "isInStock"    :true
     },
     3: {
         "id"           :200,
@@ -65,7 +68,8 @@ productList = {
         "price"        :4.99,
         "unit"         :"lb",
         "description"  :"Plain honey",
-        "increment"    :.5
+        "increment"    :.5,
+        "isInStock"    :true
     },
     4: {
         "id"           :201,
@@ -73,7 +77,8 @@ productList = {
         "price"        :5.49,
         "unit"         :"lb",
         "description"  :"Cranberry flavored honey",
-        "increment"    :.5
+        "increment"    :.5,
+        "isInStock"    :true
     },
     5: {
         "id"           :300,
@@ -81,7 +86,8 @@ productList = {
         "price"        :0.5,
         "unit"         :"&nbsp&nbsp&nbsp",
         "description"  :"",
-        "increment"    :.5
+        "increment"    :.5,
+        "isInStock"    :true
     },
     6: {
         "id"           :302,
@@ -89,7 +95,8 @@ productList = {
         "price"        :0.625,
         "unit"         :"&nbsp&nbsp&nbsp",
         "description"  :"",
-        "increment"    :0.625
+        "increment"    :0.625,
+        "isInStock"    :true
     },
     7: {
         "id"           :303,
@@ -97,7 +104,8 @@ productList = {
         "price"        :1,
         "unit"         :"&nbsp&nbsp&nbsp",
         "description"  :"",
-        "increment"    :1
+        "increment"    :1,
+        "isInStock"    :true
     },
     8: {
         "id"           :304,
@@ -105,27 +113,15 @@ productList = {
         "price"        :2,
         "unit"         :"&nbsp&nbsp&nbsp",
         "description"  :"",
-        "increment"    :2
+        "increment"    :2,
+        "isInStock"    :true
     }
 }
 
 
 module.exports.setProducts = async() => {
     for(i=0; i<Object.keys(productList).length; i++) {
-        output = await Products.findOrCreate({
-            where: {
-                id: productList[i].id
-            }, 
-            defaults: {
-                id: productList[i].id,
-                name: productList[i].name,
-                price: productList[i].price,
-                unit: productList[i].unit,
-                description: productList[i].description,
-                increment: productList[i].increment,
-                isInStock: true
-            }
-        });
-        console.log(output[0])
+        //create product if it doesn't exist, or update if it does
+        output = await Products.upsert( productList[i] );
     }
 }
