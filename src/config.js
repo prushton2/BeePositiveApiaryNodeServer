9fa5d0a928b4246bfa4771f3fd8fd474b14b48a2
prushton2 <peterrushton418@gmail.com>
2022-04-04 19:10:31 -0400

    Added pricing to the config creator

--- src/config.js
+++ src/config.js
@@ -7,6 +7,9 @@ defaultConfig = {
             "15ac60994616bb996ca08cdc4042927f032af8245485d5893388be83a16abf79"
         ]
     },
+    "pricing": {
+        "tax": 0.0625
+    },
     "sendgrid": {
         "useSendgrid": false,
         "fromEmail": "",
