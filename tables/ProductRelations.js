c4794bb890ad9a30a11d46b125e66a2dcb5a4786
prushton2 <peterrushton418@gmail.com>
2022-05-09 16:04:47 -0400

    added table to handle product relations

---
+++ tables/ProductRelations.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../src/database.js')
+const fs = require("fs")
+const util = require('util');
+const readFile = (fileName) => util.promisify(fs.readFile)(fileName, 'utf8');
+
+
+class ProductRelations extends Model {}
+
+ProductRelations.init({
+    productId: {
+      	type: DataTypes.INTEGER,
+      	primaryKey: true,
+  	},
+    subProductId: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+    },
+    price: {
+        type: DataTypes.INTEGER,
+    },
+    description: {
+        type: DataTypes.STRING,
+    },
+    unit: {
+        type: DataTypes.STRING,
+    },
+    increment: {
+        type: DataTypes.DOUBLE,
+    },
+    isInStock: {
+      type: DataTypes.BOOLEAN
+    }
+}, {
+    sequelize,
+    modelName: "productRelations",
+    timestamps: false
+})
+
+module.exports = ProductRelations;
+
+//change this? export it to json on write and import it from json on load? I need a persisntent way to store the products aside from the database (I delete it too much)
+module.exports.setProductRelations = async() => {
+    const file = await readFile("./tables/ProductRelations.json")
+    const productList = JSON.parse(file)
+    for(i=0; i<Object.keys(productList).length; i++) { 
+        //create product if it doesn't exist, or update if it does
+        output = await Products.upsert( productList[i] );
+    }
+}
