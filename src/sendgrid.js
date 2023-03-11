4d3e06b35f22064fddd3b9a637cdc317028c5c4b
prushton2 <peterrushton418@gmail.com>
2023-03-11 09:34:14 -0500

    Fixed order view url bug

--- src/sendgrid.js
+++ src/sendgrid.js
@@ -75,7 +75,7 @@ module.exports.sendOrderConfirmation = async(order, shoppingList, orderID, viewK
                     "cost": `$${totalcost.toFixed(2)}`,
                     "tax": `${100*config["pricing"]["tax"]}%`,
                     "date": date.toDateString().split(" GMT")[0],
-                    "link": `${config["domain"]["frontend-url"]}/viewOrder?orderId=${orderID}&viewKey=${viewKey}`
+                    "link": `${config["domain"]["frontend-url"]}/viewOrder?orderID=${orderID}&viewKey=${viewKey}`
                 }
             }
         ],
